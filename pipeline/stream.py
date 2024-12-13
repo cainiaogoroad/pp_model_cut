b4389dc667c12f711564277221144942b0d1fa86
cqs <1454491905@qq.com>
2024-12-13 05:29:40 +0000

    copy code from mlora

---
+++ pipeline/stream.py
@@ -0,0 +1,18 @@
+import time
+
+import torch
+
+
+class CudaStream:
+    stream_: torch.cuda.Stream
+    event_: torch.cuda.Event
+
+    def __init__(self, stream: torch.cuda.Stream) -> None:
+        self.stream_ = stream
+        self.event_ = torch.cuda.Event()
+
+    def poll(self) -> None:
+        self.event_.record(stream=self.stream_)
+        while not self.event_.query():
+            #Check if all work currently captured by event has completed.
+            time.sleep(1 / 1000)
